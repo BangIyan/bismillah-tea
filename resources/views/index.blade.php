a0e3a52b0fc4438ef202b55c0eee0674f9fbf15a
Mohammad Alviyan Anwari <mohammadalviyan17@gmail.com>
2019-01-11 14:08:06 +0700

    Init Project Admin Dashboard

---
+++ resources/views/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.home')
+<!-- Bootstrap Core CSS -->
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+<link href="/css/shop-homepage.css" rel="stylesheet">
+@section('main')
+
+    @if (!is_null($purchased_courses))
+        <h3>My courses</h3>
+        <div class="row">
+
+        @foreach($purchased_courses as $course)
+            <div class="col-sm-4 col-lg-4 col-md-4">
+                <div class="thumbnail">
+                    <img src="http://placehold.it/320x150" alt="">
+                    <div class="caption">
+                        <h4><a href="{{ route('courses.show', [$course->slug]) }}">{{ $course->title }}</a>
+                        </h4>
+                        <p>{{ $course->description }}</p>
+                    </div>
+                    <div class="ratings">
+                        <p>Progress: {{ Auth::user()->lessons()->where('course_id', $course->id)->count() }}
+                            of {{ $course->lessons->count() }} lessons</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+        </div>
+        <hr />
+
+    @endif
+
+    <h3>All courses</h3>
+    <div class="row">
+    @foreach($courses as $course)
+        <div class="col-sm-4 col-lg-4 col-md-4">
+            <div class="thumbnail">
+                <img src="http://placehold.it/320x150" alt="">
+                <div class="caption">
+                    <h4 class="pull-right">${{ $course->price }}</h4>
+                    <h4><a href="{{ route('courses.show', [$course->slug]) }}">{{ $course->title }}</a>
+                    </h4>
+                    <p>{{ $course->description }}</p>
+                </div>
+                <div class="ratings">
+                    <p class="pull-right">Students: {{ $course->students()->count() }}</p>
+                    <p>
+                        @for ($star = 1; $star <= 5; $star++)
+                            @if ($course->rating >= $star)
+                                <span class="glyphicon glyphicon-star"></span>
+                            @else
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                            @endif
+                        @endfor
+                    </p>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    </div>
+<!-- jQuery -->
+<script src="/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="/js/bootstrap.min.js"></script>
+@endsection
